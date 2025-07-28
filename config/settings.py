603256e34814ff51f0d4cab193a0147052a15dbd
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-28 12:51:12 +0100

    added herkou to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -32,11 +32,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = ['127.0.0.1']
-=======
-ALLOWED_HOSTS = ['.herokuapp.com']
->>>>>>> jos-branch
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
 
 
 # Application definition
