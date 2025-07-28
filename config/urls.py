ecc940e8fffa06c347eebefb64cced82d0b7af46
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-28 10:26:27 +0100

    added posts app

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts import views as posts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', posts_views.my_posts, name='posts'),
 ]
