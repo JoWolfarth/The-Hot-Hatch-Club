c7448245f33e12d4c7020dcc0c7ecd2a776b4aef
JoWolfarth <jose.wolfarth@btinternet.com>
2025-07-28 10:53:35 +0100

    Add URL routing and views for posts app
    - Updated config/urls.py to include posts URLs
    - Created posts/urls.py for post-related routes
    - Added home_page_view to posts/views.py

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from posts import views as posts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', posts_views.my_posts, name='posts'),
+    path("", include("posts.urls"))
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def home_page_view(request):
+    return HttpResponse("Welcome to The Hot Hatch Club Homepage!")
+
+
 def my_posts(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Welcome to The Hot Hatch Club!")
