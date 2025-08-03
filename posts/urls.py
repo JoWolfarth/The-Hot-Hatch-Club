81aeaa9f3ae46eb32b1c2fd752d5a0008fe1e912
JoWolfarth <jose.wolfarth@btinternet.com>
2025-08-03 20:52:04 +0100

    Refactor templates and styles: create base template, update index layout, and add custom CSS

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='posts'),
+    path('', views.PostList.as_view(), name='home'),
+    path('posts/', views.PostList.as_view(), name='posts'),
 ]
