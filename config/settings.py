aae383736859ced8291f2b070cac50f1dc081657
JoWolfarth <jose.wolfarth@btinternet.com>
2025-08-04 17:31:06 +0100

    added authentication

--- config/settings.py
+++ config/settings.py
@@ -43,9 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'posts',
     'django_summernote'
 ]
+
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -117,6 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('posts/', views.PostList.as_view(), name='posts'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path("accounts/", include("allauth.urls")),
 ]
